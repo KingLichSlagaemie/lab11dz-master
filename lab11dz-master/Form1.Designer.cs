0f7717da0c16c173b95f76df383bd4e1dd2c76c0
nika0-0-11 <nika_gvozdeva0.0@mail.ru>
2026-03-17 14:55:26 +0300

    first commit

---
+++ lab11dz-master/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace lab11dz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCorrect = new System.Windows.Forms.Label();
+            this.lblIncorrect = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCorrect
+            // 
+            this.lblCorrect.AutoSize = true;
+            this.lblCorrect.Location = new System.Drawing.Point(32, 18);
+            this.lblCorrect.Name = "lblCorrect";
+            this.lblCorrect.Size = new System.Drawing.Size(109, 13);
+            this.lblCorrect.TabIndex = 0;
+            this.lblCorrect.Text = "Правильных слов: 0";
+            // 
+            // lblIncorrect
+            // 
+            this.lblIncorrect.AutoSize = true;
+            this.lblIncorrect.Location = new System.Drawing.Point(198, 18);
+            this.lblIncorrect.Name = "lblIncorrect";
+            this.lblIncorrect.Size = new System.Drawing.Size(121, 13);
+            this.lblIncorrect.TabIndex = 1;
+            this.lblIncorrect.Text = "Неправильных слов: 0";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Location = new System.Drawing.Point(391, 18);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(38, 13);
+            this.lblWord.TabIndex = 2;
+            this.lblWord.Text = "Слово";
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(473, 18);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(186, 13);
+            this.lblPrompt.TabIndex = 3;
+            this.lblPrompt.Text = "Нажмите Enter чтобы ввести слово";
+            // 
+            // txtInput
+            // 
+            this.txtInput.AcceptsReturn = true;
+            this.txtInput.Location = new System.Drawing.Point(497, 76);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(122, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblIncorrect);
+            this.Controls.Add(this.lblCorrect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCorrect;
+        private System.Windows.Forms.Label lblIncorrect;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
+
---
+++ lab11dz-master/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab11dz
+{
+    public partial class Form1 : Form
+    {
+        private string[] words = { "кот", "дом", "стол", "окно", "книга", "ручка", "автомобиль", "дерево", "песня", "город" };
+        private int correct = 0;
+        private int incorrect = 0;
+        private string currentWord = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartNewWord();
+        }
+
+        private void StartNewWord()
+        {
+            Random rnd = new Random();
+            currentWord = words[rnd.Next(words.Length)];
+            lblWord.Text = currentWord;
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; 
+
+                string userInput = txtInput.Text.Trim();
+
+                if (userInput == currentWord)
+                {
+                    correct++;
+                    lblCorrect.Text = $"Правильных слов: {correct}";
+                }
+                else
+                {
+                    incorrect++;
+                    lblIncorrect.Text = $"Неправильных слов: {incorrect}";
+                }
+
+                StartNewWord();
+            }
+        }
+    }
+}
